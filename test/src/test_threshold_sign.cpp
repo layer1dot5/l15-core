aa5d7835b91d08cc4e86af1276c41450276aeae5
l2xl <l2xl@protonmail.com>
2022-07-27 23:34:39 +0300

    Signature stress test 600/300 signers

--- test/src/test_threshold_sign.cpp
+++ test/src/test_threshold_sign.cpp
@@ -3,6 +3,8 @@
 #include <cstring>
 #include <ranges>
 #include <algorithm>
+#include <random>
+#include <mutex>
 
 #include "smartinserter.hpp"
 
@@ -147,124 +149,166 @@ TEST_CASE("2-of-3 FROST signature")
     CHECK_NOTHROW(signer0.Verify(m, sig0));
 }
 
-//TEST_CASE("Keyshare 1K")
-//{
-//    const size_t N = 600;
-//    const size_t K = 300;
-//
-//    api::WalletApi wallet(api::ChainMode::MODE_REGTEST);
-//
-//
-//    std::vector<std::tuple<std::unique_ptr<SignerApi>, p2p::link_ptr>> signers;
-//    signers.reserve(N);
-//
-//    error_handler error_handler = [&](core::Error&& e) { FAIL(e.what()); };
-//
-//    std::ranges::transform(
-//       std::ranges::common_view(std::views::iota(0) | std::views::take(N)),
-//       cex::smartinserter(signers, signers.end()),
-//       [&](int i) {
-//           std::unique_ptr<SignerApi> signer(
-//                   new SignerApi(wallet, i, ChannelKeys(wallet), N, K, error_handler));
-//           p2p::link_ptr link = p2p::link_ptr(new p2p::LocalLink(*signer));
-//           return std::tuple<std::unique_ptr<SignerApi>, p2p::link_ptr>(std::move(signer), std::move(link));
-//       }
-//    );
-//
-//    std::clog << "Signers are initialized" << std::endl;
-//
-//    aggregate_key_handler empty_handler = [](SignerApi& s) { };
-//
-//    TimeMeasure reg_measure("Exchange peer pubkeys");
-//
-//    std::for_each(std::execution::par_unseq, signers.begin(), signers.end(), [&](auto &si) {
-//        reg_measure.Measure([&]() {
-//            for(auto &sj: signers)
-//            {
-//                if(si != sj)
-//                {
-//                    std::get<0>(si)->AddPeer(std::get<0>(sj)->GetIndex(), std::get<1>(sj));
-//                }
-//            }
-//            std::get<0>(si)->RegisterToPeers(empty_handler);
-//            return 0;
-//        });
-//    });
-//
-//    reg_measure.Report(std::clog);
-//
-//
-//    TimeMeasure distrib_measure("Exchange Key shares");
-//
-//    std::for_each(signers.begin(), signers.end(), [&](auto& s){
-//        distrib_measure.Measure([&](){
-//            std::get<0>(s)->DistributeKeyShares();
-//            if(std::get<0>(s)->GetIndex() % 100 == 99)
-//            {
-//                std::clog << "Peer " << std::get<0>(s)->GetIndex() << " key share completed" << std::endl;
-//            }
-//            return 0;
-//        });
-//    });
-//
-//    distrib_measure.Report(std::clog);
-//
-//    TimeMeasure keyagg_measure("Aggregate pubkey");
-//    std::for_each(std::execution::par_unseq, signers.begin(), signers.end(), [&](auto &si) {
-//        keyagg_measure.Measure([&]() {
-//            std::get<0>(si)->AggregateKey();
-//            return 0;
-//        });
-//    });
-//    keyagg_measure.Report(std::clog);
-//
-//    const auto& pubkey0 = std::get<0>(signers.front())->GetAggregatedPubKey();
-//    CHECK_FALSE(ChannelKeys::IsZeroArray(pubkey0));
-//    for(const auto& si: signers)
-//    {
-//        CHECK(HexStr(pubkey0) == HexStr(std::get<0>(si)->GetAggregatedPubKey()));
-//    }
-//
-//    TimeMeasure nonceshare_measure("Commit nonces");
-//    std::for_each(std::execution::par_unseq, signers.begin(), signers.end(), [&](auto &si) {
-//        nonceshare_measure.Measure([&]() {
-//            std::get<0>(si)->CommitNonces(1);
-//            return 0;
-//        });
-//    });
-//    nonceshare_measure.Report(std::clog);
-//
-//    //CHECK(std::get<0>(signers.front())->Peers()[0].ephemeral_pubkeys.size() == 1);
-//
-//    bytevector message_data32 {'T','h','i','s',' ','i','s',' ','t','e','s','t',' ','d','a','t','a',' ','t','o',' ','b','e',' ','s','i','g','n','e','d','!','!'};
-//    REQUIRE(message_data32.size() == 32);
-//    uint256 m(message_data32);
-//
-//    TimeMeasure initsig_measure("Init signature");
-//    std::for_each(std::execution::par_unseq, signers.begin(), signers.end(), [&](auto &si) {
-//        initsig_measure.Measure([&]() {
-//            std::get<0>(si)->InitSignature(0, m, [](SignerApi& s) {});
-//            return 0;
-//        });
-//    });
-//    initsig_measure.Report(std::clog);
-//
-//    TimeMeasure distribsig_measure("Distribute sig shares");
-//    std::for_each(std::execution::par_unseq, signers.begin(), signers.end(), [&](auto &si) {
-//        distribsig_measure.Measure([&]() {
-//            std::get<0>(si)->DistributeSigShares();
-//            return 0;
-//        });
-//    });
-//    distribsig_measure.Report(std::clog);
-//
-//    TimeMeasure aggsig_measure("Aggregate signature");
-//    std::for_each(std::execution::par_unseq, signers.begin(), signers.end(), [&](auto &si) {
-//        initsig_measure.Measure([&]() {
-//            std::get<0>(si)->AggregateSignature();
-//            return 0;
-//        });
-//    });
-//    initsig_measure.Report(std::clog);
-//
-//}
+TEST_CASE("Keyshare 1K")
+{
+    const size_t N = 600;
+    const size_t K = 300;
+
+    api::WalletApi wallet(api::ChainMode::MODE_REGTEST);
+
+
+    std::vector<std::tuple<std::unique_ptr<SignerApi>, p2p::link_ptr>> signers;
+    signers.reserve(N);
+
+    aggregate_key_handler key_hdl = [](SignerApi& s) { s.AggregateKey(); };
+    new_sigop_handler new_sigop_hdl = [](SignerApi&, operation_id) { };
+    aggregate_sig_handler sig_hdl = [](SignerApi&, operation_id) { };
+    error_handler error_hdl = [&](core::Error&& e) { FAIL(e.what()); };
+
+    std::ranges::transform(
+       std::ranges::common_view(std::views::iota(0) | std::views::take(N)),
+       cex::smartinserter(signers, signers.end()),
+       [&](int i) {
+           std::unique_ptr<SignerApi> signer(
+                   new SignerApi(wallet, i, ChannelKeys(wallet), N, K, new_sigop_hdl, sig_hdl, error_hdl));
+           p2p::link_ptr link = p2p::link_ptr(new p2p::LocalLink(*signer));
+           return std::tuple<std::unique_ptr<SignerApi>, p2p::link_ptr>(std::move(signer), std::move(link));
+       }
+    );
+
+    std::clog << "Signers are initialized" << std::endl;
+
+    aggregate_key_handler empty_handler = [](SignerApi& s) { };
+
+    {
+        TimeMeasure reg_measure("Exchange peer pubkeys");
+        std::for_each(std::execution::par_unseq, signers.begin(), signers.end(), [&](auto &si) {
+            reg_measure.Measure([&]() {
+                for (auto &sj: signers) {
+                    if (si != sj) {
+                        std::get<0>(si)->AddPeer(std::get<0>(sj)->GetIndex(), std::get<1>(sj));
+                    }
+                }
+                std::get<0>(si)->RegisterToPeers(empty_handler);
+                return 0;
+            });
+        });
+        reg_measure.Report(std::clog);
+    }
+
+
+    {
+        TimeMeasure distrib_measure("Exchange Key shares");
+        std::for_each(signers.begin(), signers.end(), [&](auto& s){
+            distrib_measure.Measure([&](){
+                std::get<0>(s)->DistributeKeyShares();
+                if(std::get<0>(s)->GetIndex() % 100 == 99)
+                {
+                    std::clog << "Peer " << std::get<0>(s)->GetIndex() << " key share completed" << std::endl;
+                }
+                return 0;
+            });
+        });
+        distrib_measure.Report(std::clog);
+    }
+
+    {
+        TimeMeasure keyagg_measure("Aggregate pubkey");
+        std::for_each(std::execution::par_unseq, signers.begin(), signers.end(), [&](auto &si) {
+            keyagg_measure.Measure([&]() {
+                std::get<0>(si)->AggregateKey();
+                return 0;
+            });
+        });
+        keyagg_measure.Report(std::clog);
+    }
+
+    const auto& pubkey0 = std::get<0>(signers.front())->GetAggregatedPubKey();
+    CHECK_FALSE(ChannelKeys::IsZeroArray(pubkey0));
+    for(const auto& si: signers) {
+        CHECK(HexStr(pubkey0) == HexStr(std::get<0>(si)->GetAggregatedPubKey()));
+    }
+
+    {
+        TimeMeasure nonceshare_measure("Commit nonces");
+        std::for_each(std::execution::par_unseq, signers.begin(), signers.end(), [&](auto &si) {
+            nonceshare_measure.Measure([&]() {
+                std::get<0>(si)->CommitNonces(1);
+                return 0;
+            });
+        });
+        nonceshare_measure.Report(std::clog);
+    }
+
+    //CHECK(std::get<0>(signers.front())->Peers()[0].ephemeral_pubkeys.size() == 1);
+
+    bytevector message_data32 {'T','h','i','s',' ','i','s',' ','t','e','s','t',' ','d','a','t','a',' ','t','o',' ','b','e',' ','s','i','g','n','e','d','!','!'};
+    REQUIRE(message_data32.size() == 32);
+    uint256 m(message_data32);
+
+    std::set<size_t> actual_signers;
+
+    std::random_device r;
+    std::default_random_engine e1(r());
+    std::uniform_int_distribution<size_t> uniform_dist(0, N-1);
+
+    while (actual_signers.size() < K) {
+        actual_signers.emplace(uniform_dist(e1));
+    }
+
+    {
+        TimeMeasure initsig_measure("Init signature");
+        std::for_each(std::execution::par_unseq, actual_signers.begin(), actual_signers.end(), [&](auto &i) {
+            initsig_measure.Measure([&]() {
+                std::get<0>(signers[i])->InitSignature(0);
+                return 0;
+            });
+        });
+        initsig_measure.Report(std::clog);
+    }
+
+    {
+        TimeMeasure preprocsig_measure("Preprocess signature");
+        std::for_each(std::execution::par_unseq, actual_signers.begin(), actual_signers.end(), [&](auto &i) {
+            preprocsig_measure.Measure([&]() {
+                std::get<0>(signers[i])->PreprocessSignature(m, 0);
+                return 0;
+            });
+        });
+        preprocsig_measure.Report(std::clog);
+    }
+
+    {
+        TimeMeasure distribsig_measure("Distribute sig shares");
+        std::for_each(std::execution::par_unseq, actual_signers.begin(), actual_signers.end(), [&](auto &i) {
+            distribsig_measure.Measure([&]() {
+                std::get<0>(signers[i])->DistributeSigShares(0);
+                return 0;
+            });
+        });
+        distribsig_measure.Report(std::clog);
+    }
+
+    {
+        std::mutex sig_mutex;
+        std::list<signature> final_sigs;
+        TimeMeasure aggsig_measure("Aggregate signature");
+        std::for_each(std::execution::par_unseq, actual_signers.begin(), actual_signers.end(), [&](auto &i) {
+            aggsig_measure.Measure([&]() {
+                signature sig = std::get<0>(signers[i])->AggregateSignature(0);
+                {
+                    [[maybe_unused]] const std::lock_guard<std::mutex> lock(sig_mutex);
+                    final_sigs.emplace_back(std::move(sig));
+                }
+                return 0;
+            });
+        });
+        aggsig_measure.Report(std::clog);
+
+        for (const auto& sig: final_sigs) {
+            CHECK_NOTHROW(std::get<0>(signers.front())->Verify(m, sig));
+        }
+
+    }
+
+}
