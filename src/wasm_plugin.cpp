528229cb4fa6949e2ca63f0020f05d199f85d9e7
l2xl <l2xl@protonmail.com>
2023-04-08 22:07:17 +0300

    Inscription Swap contract - embind interface

--- src/wasm_plugin.cpp
+++ src/wasm_plugin.cpp
@@ -5,6 +5,7 @@
 #include "common.hpp"
 #include "channel_keys.hpp"
 #include "create_inscription.hpp"
+#include "swap_inscription.hpp"
 
 #include <emscripten.h>
 #include <emscripten/bind.h>
@@ -69,14 +70,50 @@ EMSCRIPTEN_BINDINGS(inscribeit) {
             .property(l15::inscribeit::CreateInscriptionBuilder::name_inscribe_script_pk.c_str(), &l15::inscribeit::CreateInscriptionBuilder::GetInscribeScriptPubKey)
             .property(l15::inscribeit::CreateInscriptionBuilder::name_inscribe_sig.c_str(), &l15::inscribeit::CreateInscriptionBuilder::GetInscribeScriptSig)
             .property(l15::inscribeit::CreateInscriptionBuilder::name_inscribe_int_pk.c_str(), &l15::inscribeit::CreateInscriptionBuilder::GetInscribeInternaltPubKey)
-//                .function("UTXO", &l15::inscribeit::CreateInscriptionBuilder::UTXO)
-//                .function("Data", &l15::inscribeit::CreateInscriptionBuilder::Data)
-//                .function("FeeRate", &l15::inscribeit::CreateInscriptionBuilder::FeeRate)
-//                .function("PrivKeys", &l15::inscribeit::CreateInscriptionBuilder::PrivKeys)
+
             .function("Sign", &l15::inscribeit::CreateInscriptionBuilder::Sign)
             .function("Serialize", &l15::inscribeit::CreateInscriptionBuilder::Serialize)
 
             .property("intermediate_taproot_pk", &l15::inscribeit::CreateInscriptionBuilder::IntermediateTaprootPrivKey)
+            ;
+
+    emscripten::enum_<l15::inscribeit::SwapInscriptionBuilder::SwapPhase>("SwapPhase")
+            .value("ORD_TERMS", l15::inscribeit::SwapInscriptionBuilder::OrdTerms)
+            .value("ORD_COMMIT_SIG", l15::inscribeit::SwapInscriptionBuilder::OrdCommitSig)
+            .value("FUNDS_TERMS", l15::inscribeit::SwapInscriptionBuilder::FundsTerms)
+            .value("FUNDS_COMMIT_SIG", l15::inscribeit::SwapInscriptionBuilder::FundsCommitSig)
+            .value("ORD_PAYOFF_TERMS", l15::inscribeit::SwapInscriptionBuilder::MarketPayoffTerms)
+            .value("ORD_PAYOFF_SIG", l15::inscribeit::SwapInscriptionBuilder::MarketPayoffSig)
+            .value("ORD_SWAP_SIG", l15::inscribeit::SwapInscriptionBuilder::OrdSwapSig)
+            .value("FUNDS_SWAP_SIG", l15::inscribeit::SwapInscriptionBuilder::FundsSwapSig)
+            .value("MARKET_SWAP_SIG", l15::inscribeit::SwapInscriptionBuilder::MarketSwapSig)
+            ;
+
+    emscripten::class_<l15::inscribeit::SwapInscriptionBuilder>("SwapInscriptionBuilder")
+            .constructor<std::string, std::string, std::string>()
+            .property(l15::inscribeit::ContractBuilder::name_version.c_str(), &l15::inscribeit::SwapInscriptionBuilder::GetProtocolVersion)
+            .property(l15::inscribeit::SwapInscriptionBuilder::name_mining_fee_rate.c_str(), &l15::inscribeit::SwapInscriptionBuilder::GetMiningFeeRate, &l15::inscribeit::SwapInscriptionBuilder::SetMiningFeeRate)
+            .property(l15::inscribeit::SwapInscriptionBuilder::name_ord_commit_mining_fee_rate.c_str(), &l15::inscribeit::SwapInscriptionBuilder::GetOrdCommitMiningFeeRate, &l15::inscribeit::SwapInscriptionBuilder::SetOrdCommitMiningFeeRate)
+            .property(l15::inscribeit::SwapInscriptionBuilder::name_swap_script_pk_A.c_str(), &l15::inscribeit::SwapInscriptionBuilder::GetSwapScriptPubKeyA, &l15::inscribeit::SwapInscriptionBuilder::SetSwapScriptPubKeyA)
+            .property(l15::inscribeit::SwapInscriptionBuilder::name_swap_script_pk_B.c_str(), &l15::inscribeit::SwapInscriptionBuilder::GetSwapScriptPubKeyB, &l15::inscribeit::SwapInscriptionBuilder::SetSwapScriptPubKeyB)
+            .property(l15::inscribeit::SwapInscriptionBuilder::name_swap_script_pk_M.c_str(), &l15::inscribeit::SwapInscriptionBuilder::GetSwapScriptPubKeyM, &l15::inscribeit::SwapInscriptionBuilder::SetSwapScriptPubKeyM)
+            .property(l15::inscribeit::SwapInscriptionBuilder::name_swap_hash.c_str(), &l15::inscribeit::SwapInscriptionBuilder::GetSwapHash, &l15::inscribeit::SwapInscriptionBuilder::SetSwapHash)
+            .property(l15::inscribeit::SwapInscriptionBuilder::name_ord_txid.c_str(), &l15::inscribeit::SwapInscriptionBuilder::GetOrdUtxoTxId, &l15::inscribeit::SwapInscriptionBuilder::SetOrdUtxoTxId)
+            .property(l15::inscribeit::SwapInscriptionBuilder::name_ord_nout.c_str(), &l15::inscribeit::SwapInscriptionBuilder::GetOrdUtxoNOut, &l15::inscribeit::SwapInscriptionBuilder::SetOrdUtxoNOut)
+            .property(l15::inscribeit::SwapInscriptionBuilder::name_ord_amount.c_str(), &l15::inscribeit::SwapInscriptionBuilder::GetOrdUtxoAmount, &l15::inscribeit::SwapInscriptionBuilder::SetOrdUtxoAmount)
+            .property(l15::inscribeit::SwapInscriptionBuilder::name_funds_txid.c_str(), &l15::inscribeit::SwapInscriptionBuilder::GetFundsUtxoTxId, &l15::inscribeit::SwapInscriptionBuilder::SetFundsUtxoTxId)
+            .property(l15::inscribeit::SwapInscriptionBuilder::name_funds_nout.c_str(), &l15::inscribeit::SwapInscriptionBuilder::GetFundsUtxoNOut, &l15::inscribeit::SwapInscriptionBuilder::SetFundsUtxoNOut)
+            .property(l15::inscribeit::SwapInscriptionBuilder::name_funds_amount.c_str(), &l15::inscribeit::SwapInscriptionBuilder::GetFundsUtxoAmount, &l15::inscribeit::SwapInscriptionBuilder::SetFundsUtxoAmount)
+
+            .function("SignOrdCommitment", &l15::inscribeit::SwapInscriptionBuilder::SignOrdCommitment)
+            .function("SignOrdPayBack", &l15::inscribeit::SwapInscriptionBuilder::SignOrdPayBack)
+            .function("SignOrdSwap", &l15::inscribeit::SwapInscriptionBuilder::SignOrdSwap)
+            .function("SignFundsCommitment", &l15::inscribeit::SwapInscriptionBuilder::SignFundsCommitment)
+            .function("SignFundsPayBackt", &l15::inscribeit::SwapInscriptionBuilder::SignFundsPayBack)
+            .function("SignFundsSwap", &l15::inscribeit::SwapInscriptionBuilder::SignFundsSwap)
+
+            .function("Serialize", &l15::inscribeit::SwapInscriptionBuilder::Serialize)
+            .function("Deserialize", &l15::inscribeit::SwapInscriptionBuilder::Deserialize)
             ;
 
     emscripten::class_<ChannelKeysWasm>("ChannelKeys")
